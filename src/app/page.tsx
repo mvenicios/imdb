b833906a9efdc23b8b01d6dca2ca4aa3ed76a08d
mauro <mvenicios@bol.com.br>
2023-04-24 15:08:51 -0300

    handle the error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,23 @@
-import Image from "next/image";
+import Results from "./components/Results";
+const API_KEY = process.env.API_KEY;
 
-export default function Home() {
-  return <h1 className="text-red-400">Home</h1>;
+export default async function Home({ searchParams }) {
+  const genre = searchParams.genre || "fetchTrending";
+  const res = await fetch(
+    `https://api.themoviedb.org/3/${
+      genre === "fetchTopRated" ? "movie/top_rated" : "trending/all/week"
+    }?api_key=${API_KEY}&language=pt-BR&page=1`,
+    { next: { revalidate: 10000 } }
+  );
+  if (!res.ok) {
+    throw new Error("Falha ao buscar os dados.");
+  }
+  const data = await res.json();
+  const results = data.results;
+
+  return (
+    <div>
+      <Results results={results} />
+    </div>
+  );
 }
