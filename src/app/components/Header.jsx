3374153395e9182e59eb621cc0f04e7050535e36
mauro <mvenicios@bol.com.br>
2023-04-23 22:12:54 -0300

    create the about page

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -6,7 +6,7 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <div className="flex justify-between mx-2 max-w-6x1 sm:mx-auto items-center py-6">
+    <div className="flex justify-between mx-2 max-w-6xl sm:mx-auto items-center py-6">
       <div className="flex">
         <MenuItem title="Home" address="/" Icon={AiFillHome} />
         <MenuItem title="Sobre" address="/about" Icon={BsFillInfoCircleFill} />
@@ -14,11 +14,11 @@ export default function Header() {
       <div className=" flex items-center space-x-5">
         <DarkModeSwitch />
         <Link href="/">
-          <h2 className="text-2x1">
+          <h2 className="text-2xl">
             <span className="font-bold bg-amber-500 py-1 px-2 rounded-lg mr-1">
               IMDb
             </span>
-            <span className="text-x1 hidden sm:inline">Clone</span>
+            <span className="text-x-1 hidden sm:inline">Clone</span>
           </h2>
         </Link>
       </div>
